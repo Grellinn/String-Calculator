38136db83ce5cc421e018c6a6614df12929018ed
Gretar Mar <grellinn@gmail.com>
2016-10-13 20:41:54 +0000

    Eight test added: stringWithNegativeNumber

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -46,4 +46,10 @@ public class CalculatorTest {
     {
         assertEquals(6, Calculator.add("1\n2,3"));
     }
+
+    @Test
+    public void stringWithNegativeNumber()
+    {
+        assertEquals("Negatives not allowed: -1", Calculator.add("-1,2"));
+    }
 }
